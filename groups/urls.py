21d086ea13fd748d575dc9bbe8fbd3fb7d4325c9
manoel <candidomanoel78@gmail.com>
2026-05-10 01:05:15 -0300

    feat: adiciona rota para deixar grupo

--- groups/urls.py
+++ groups/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from groups.views import GroupCreateView, GroupListView, GroupJoinView
+from groups.views import GroupCreateView, GroupListView, GroupJoinView, GroupLeaveView
 
 
 urlpatterns = [
     path('groups/create/', GroupCreateView.as_view(), name='group-create'),
     path('groups/list/', GroupListView.as_view(), name='group-list'),
     path('groups/join/', GroupJoinView.as_view(), name='group-join'),
+    path('groups/leave/<int:pk>', GroupLeaveView.as_view(), name='group-leave'),
 ]

e768a674295c24a26b8573c2977dd4e8e5d5025f
manoel <candidomanoel78@gmail.com>
2026-05-09 23:01:15 -0300

    feat: adiciona model guess em guesses

--- guesses/models.py
+++ guesses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import CustomUser
+from groups.models import Group
+from matches.models import Match
 
-# Create your models here.
+
+class Guess(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='guesses')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='guesses')
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='guesses')
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+    points = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'group', 'match')

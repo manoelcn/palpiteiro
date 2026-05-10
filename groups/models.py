5342eb51092e98db5b88fdcd8873c02ff3e3fcb6
manoel <candidomanoel78@gmail.com>
2026-05-09 22:44:37 -0300

    feat: adiciona models group e membership em groups

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from accounts.models import CustomUser
+import secrets
+import string
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    invite_code = models.CharField(unique=True, max_length=5)
+    owner = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name='owned_groups')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def generate_code(size=5):
+        characters = string.ascii_lowercase + string.digits
+        code = ''.join(secrets.choice(characters) for _ in range(size))
+        return code
+
+    def save(self, *args, **kwargs):
+        if not self.invite_code:
+            self.invite_code = self.generate_code()
+        return super().save(*args, **kwargs)
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name='memberships')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='memberships')
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('group', 'user',)

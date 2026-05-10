18ad4410c66cd6c2640d4fd1e4bd87d7222df4e3
manoel <candidomanoel78@gmail.com>
2026-05-10 00:07:35 -0300

    feat: cria group create view

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from groups.models import Group
+from groups.forms import GroupForm
 
-# Create your views here.
+class GroupCreateView(LoginRequiredMixin, CreateView):
+    model = Group
+    template_name = 'group_create.html'
+    form_class = GroupForm
+    success_url = reverse_lazy('group-list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)

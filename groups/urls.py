3e2aa453b805898228fcfacc64ae3f3f46cbcd09
manoel <candidomanoel78@gmail.com>
2026-05-10 00:56:13 -0300

    feat: cria url para group join

--- groups/urls.py
+++ groups/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from groups.views import GroupCreateView, GroupListView
+from groups.views import GroupCreateView, GroupListView, GroupJoinView
 
 
 urlpatterns = [
     path('groups/create/', GroupCreateView.as_view(), name='group-create'),
     path('groups/list/', GroupListView.as_view(), name='group-list'),
+    path('groups/join/', GroupJoinView.as_view(), name='group-join'),
 ]

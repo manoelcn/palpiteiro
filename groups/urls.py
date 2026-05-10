6acb7bbef69e2a34d70f5f1e4e08e95cb97e3918
manoel <candidomanoel78@gmail.com>
2026-05-10 00:34:32 -0300

    feat: cria rota de listar grupos

--- groups/urls.py
+++ groups/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from groups.views import GroupCreateView
+from groups.views import GroupCreateView, GroupListView
 
 
 urlpatterns = [
     path('groups/create/', GroupCreateView.as_view(), name='group-create'),
+    path('groups/list/', GroupListView.as_view(), name='group-list'),
 ]

07e93296899d57bc676612c0322d8f5c59907108
manoel <candidomanoel78@gmail.com>
2026-05-09 22:54:05 -0300

    feat: adiciona model match em matches

--- matches/models.py
+++ matches/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICE = [
+    ('scheduled', 'scheduled'),
+    ('in_progress', 'in_progress'),
+    ('finished', 'finished'),
+]
+
+class Match(models.Model):
+    home_team = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    kickoff = models.DateTimeField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICE, default='scheduled')
+    home_score = models.IntegerField(null=True)
+    away_score = models.IntegerField(null=True)
+    api_match_id = models.CharField(max_length=100)

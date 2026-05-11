9aa656e1952a81843b1b7a5580d12d05511cae86
manoel <candidomanoel78@gmail.com>
2026-05-10 23:14:03 -0300

    feat: adiciona rota de atualizar palpite

--- guesses/urls.py
+++ guesses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from guesses.views import GuessCreateView
+from guesses.views import GuessCreateView, GuessUpdateView
 
 
 urlpatterns = [
     path('guesses/<int:group_pk>/<int:match_pk>/create/', GuessCreateView.as_view(), name='guess-create'),
+    path('guesses/<int:group_pk>/<int:pk>/update/', GuessUpdateView.as_view(), name='guess-update'),
 ]

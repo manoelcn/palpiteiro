8a134df02ab13f6c8b4444a14cde6b40b8f05a44
manoel <candidomanoel78@gmail.com>
2026-05-10 22:38:38 -0300

    feat: adiciona rota de detail group

--- groups/urls.py
+++ groups/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from groups.views import GroupCreateView, GroupListView, GroupJoinView, GroupLeaveView
+from groups.views import GroupCreateView, GroupListView, GroupJoinView, GroupLeaveView, GroupDetailView
 
 
 urlpatterns = [
     path('groups/create/', GroupCreateView.as_view(), name='group-create'),
     path('groups/list/', GroupListView.as_view(), name='group-list'),
+    path('groups/<int:pk>/', GroupDetailView.as_view(), name='group-detail'),
     path('groups/join/', GroupJoinView.as_view(), name='group-join'),
     path('groups/leave/<int:pk>', GroupLeaveView.as_view(), name='group-leave'),
 ]

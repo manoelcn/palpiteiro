592b016a5715b3635e64cf37e8cc3345894aad4d
manoel <candidomanoel78@gmail.com>
2026-05-10 23:22:47 -0300

    feat: adiciona url de atualizar grupo

--- groups/urls.py
+++ groups/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from groups.views import GroupCreateView, GroupListView, GroupJoinView, GroupLeaveView, GroupDetailView
+from groups.views import GroupCreateView, GroupListView, GroupJoinView, GroupLeaveView, GroupDetailView, GroupUpdateView
 
 
 urlpatterns = [
     path('groups/create/', GroupCreateView.as_view(), name='group-create'),
+    path('groups/<int:pk>/update/', GroupUpdateView.as_view(), name='group-update'),
     path('groups/list/', GroupListView.as_view(), name='group-list'),
     path('groups/<int:pk>/', GroupDetailView.as_view(), name='group-detail'),
     path('groups/join/', GroupJoinView.as_view(), name='group-join'),

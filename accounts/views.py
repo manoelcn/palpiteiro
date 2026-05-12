765e288b8610b6ca167f73a1dc6b1283a6bfd600
manoel <candidomanoel78@gmail.com>
2026-05-11 23:55:04 -0300

    feat: faz login automático após cadastro de usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 from django.views.generic import CreateView
 from accounts.forms import AccountForm
 from accounts.models import CustomUser
@@ -9,4 +10,9 @@ class AccountRegister(CreateView):
     model = CustomUser
     template_name = 'account_create.html'
     form_class = AccountForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response

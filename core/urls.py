7e06eefcbe6e6fbfbaec6dd865989ab732e4496e
manoel <candidomanoel78@gmail.com>
2026-05-11 22:39:35 -0300

    feat: adiciona rota do dashboard

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core.views import CoreView
+from core.views import CoreView, DashboardView
 
 
 urlpatterns = [
     path('', CoreView.as_view(), name='home'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]

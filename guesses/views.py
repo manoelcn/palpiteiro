79aea261602e3d2eb3af609496b460c4c82eeb08
manoel <candidomanoel78@gmail.com>
2026-05-11 22:54:45 -0300

    feat: adiciona mensagens de palpite salvo e atualizado

--- guesses/views.py
+++ guesses/views.py
@@ -18,7 +18,9 @@ class GuessCreateView(LoginRequiredMixin, CreateView):
         form.instance.group_id = self.kwargs['group_pk']
         form.instance.match_id = self.kwargs['match_pk']
         try:
-            return super().form_valid(form)
+            response = super().form_valid(form)
+            messages.success(self.request, 'Palpite salvo com sucesso!')
+            return response
         except IntegrityError:
             messages.error(self.request, 'Você já fez um palpite para esse jogo!')
             return self.form_invalid(form)
@@ -37,3 +39,7 @@ class GuessUpdateView(LoginRequiredMixin, UpdateView):
     
     def get_success_url(self):
         return reverse_lazy('group-detail', kwargs={'pk': self.kwargs['group_pk']})
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Palpite atualizado com sucesso!')
+        return super().form_valid(form)

5239dfebed98841a2b4379112fe79e51c9370fa3
manoel cândido <candidomanoel78@gmail.com>
2026-06-11 13:28:44 -0300

    feat: validação para adicionar apenas jogos que ainda não aconteceram

--- groups/forms.py
+++ groups/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.db.models import Q
 from groups.models import Group
+from matches.models import Match
 
 
 class GroupForm(forms.ModelForm):
@@ -20,6 +22,17 @@ class GroupForm(forms.ModelForm):
                 'size': '8',
             }),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        if 'matches' in self.fields:
+            if not self.instance.pk:
+                self.fields['matches'].queryset = Match.objects.filter(status='TIMED')
+            else:
+                self.fields['matches'].queryset = Match.objects.filter(
+                    Q(status='TIMED') | Q(id__in=self.instance.matches.all())
+                ).distinct()
 
 
 class InviteCodeForm(forms.Form):

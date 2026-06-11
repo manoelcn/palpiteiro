b8c7c4450c6bb076c9b5a9994682415860d3e2d1
manoel cândido <candidomanoel78@gmail.com>
2026-06-11 13:11:21 -0300

    feat: registra groups no admin

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Group, Membership
 
-# Register your models here.
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'invite_code', 'owner', 'created_at')
+    search_fields = ('name', 'invite_code', 'owner__username')
+    readonly_fields = ('invite_code',)
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'group', 'joined_at')
+    search_fields = ('user__username', 'group__name')
+    list_filter = ('group',)
